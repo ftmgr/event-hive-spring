f08403b287a574d738602ed6390900fdf2298acf
Fatma <fatmagurses1@gmail.com>
2024-05-27 10:46:39 +0300

    Initial commit

---
+++ src/test/java/com/project/event_hive_spring/EventHiveSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.event_hive_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventHiveSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
